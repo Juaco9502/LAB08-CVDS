6d21b753985d04185ac36b8a9fc9ff2a1ad447e5
Juaco9502 <juaco9602@hotmail.com>
2020-03-22 14:06:12 -0500

    Avance laboratorio08

---
+++ src/main/java/edu/eci/cvds/sampleprj/dao/ClienteDAO.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cvds.sampleprj.dao;
+
+import edu.eci.cvds.samples.entities.Cliente;
+import java.util.*;
+import org.mybatis.guice.transactional.Transactional;
+
+/**
+ *
+ * @author 2105516
+ */
+public interface ClienteDAO {
+    
+    public Cliente load(long id) throws PersistenceException;
+	
+	@Transactional
+	public void addItemRentado(int idC,int idit, Date fechainicio,Date fechafin) throws PersistenceException;
+
+	public List<Cliente> loadAll() throws PersistenceException;
+
+	
+	@Transactional
+	public void setVetado(long docu, boolean estado) throws PersistenceException;
+	
+	public void addCliente(Cliente c) throws PersistenceException;
+    
+}
---
+++ src/main/java/edu/eci/cvds/sampleprj/dao/TipoItemDAO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cvds.sampleprj.dao;
+
+import edu.eci.cvds.samples.entities.TipoItem;
+import java.util.*;
+import org.mybatis.guice.transactional.Transactional;
+
+/**
+ *
+ * @author 2105516
+ */
+public interface TipoItemDAO {
+    
+    public TipoItem load(int id) throws PersistenceException;
+    
+    @Transactional
+    public void save(String des) throws PersistenceException;
+    public List<TipoItem> loadAll() throws PersistenceException;
+    
+}
